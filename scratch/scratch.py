75f75c85c8ea17a871f6bf1b6dffcab42f6d167b
Alexandros Popov <alexandros.popov@deepomatic.com>
2025-06-15 17:18:38 +0200

    wip agents.

--- scratch/scratch.py
+++ scratch/scratch.py
@@ -6,7 +6,9 @@ from smolagents.models import InferenceClientModel   # Hugging Face Inference AP
 from smolagents import ToolCollection, LiteLLMModel
 # Create your views here.
 import os
-
+import tempfile
+import matplotlib.pyplot as plt
+from smolagents import tool
 
 REMOTE_MCP = {
     "url": "http://localhost:5000/mcp/",
@@ -30,16 +32,52 @@ def build_model() -> InferenceClientModel:
     return LiteLLMModel(model_id="claude-sonnet-4-20250514", api_key=os.environ['ANTHROPIC_API_KEY'])
 
 
+@tool
+def save_matplotlib_graph_to_tmp() -> None:
+    """
+    Saves the current matplotlib figure to a uniquely named temporary PNG file.
+
+        Returns:
+            str: The file path of the saved temporary PNG file.
+    """
+    tmp_file = tempfile.NamedTemporaryFile(suffix=".png", delete=False)
+    plt.savefig(tmp_file.name)
+    tmp_file.close()
+    return tmp_file.name
+    
+
 def run_agent(tools_cfg, prompt: str) -> str:
     with ToolCollection.from_mcp(tools_cfg, trust_remote_code=True) as tool_collection:
-        agent = CodeAgent(tools=[*tool_collection.tools], model=build_model(), add_base_tools=False)
-        result = agent.run(prompt)
+        retriever_prompt="""
+        While processing the following request, work with a DataFrame called working_df. 
+        When you are done, convert the dataframe to json with working_df.to_json, and return it. 
+        When ever you aquire data, you must aquire at least 100 posts, or comments.
+        """
 
-        agent = CodeAgent(tools=[*tool_collection.tools], 
+        agent_retriever = CodeAgent(tools=[*tool_collection.tools], model=build_model(), add_base_tools=False, additional_authorized_imports=["pandas", "pandas.*", "json"])
+        data_extract = agent_retriever.run(retriever_prompt + f"\nuser request: {prompt}" )
+
+
+        agent_analyzer = CodeAgent(tools=[*tool_collection.tools], 
                           model=build_model(), 
-                          add_base_tools=False,
-                          additional_args={"dataframe": result})
-        result2 = agent.run("analyze this json dataset and give me interesting information")
+                          add_base_tools=False, additional_authorized_imports=["pandas", "pandas.*", "json"])
+        
+        intereting_info = agent_analyzer.run("analyze this pandas dataset and give me interesting information.",
+                            additional_args={
+                                "dataframe": data_extract
+                            })
+        
+        graph_agent = CodeAgent(tools=[*tool_collection.tools, save_matplotlib_graph_to_tmp], 
+                          model=build_model(), 
+                          add_base_tools=False, additional_authorized_imports=["pandas", "pandas.*", "json", "seaborn", "matplotlib.*"])
+        
+        graph = graph_agent.run("come up with directions to represent this information as graphs"
+                                "you are given a pandas frame as well as directions to make graphs out of this pandas frame",
+                            additional_args={
+                                "dataframe": data_extract,
+                                "directions": intereting_info 
+                            })
+
 
     # mcp_client = MCPClient([tools_cfg])
     # with MCPClient(tools_cfg) as mcp_client:
@@ -52,4 +90,6 @@ def run_agent(tools_cfg, prompt: str) -> str:
     #     return result
 
 
-run_agent(REMOTE_MCP, "give me the best voted posts on r/politics")
+run_agent(REMOTE_MCP, "give me the best voted posts on r/politics over the last week. you must return the information as a pandas dataframe, or you will die")
+
+
